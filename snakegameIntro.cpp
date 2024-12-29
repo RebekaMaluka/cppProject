b50c5aeeb900cfc5d1ff6c691f2fb148245cf905
rebekamaluka <rebecalawrence84@gmail.com>
2024-12-29 20:07:28 +0200

    functions

--- snakegameIntro.cpp
+++ snakegameIntro.cpp
@@ -43,6 +43,50 @@ bool isGameOver;
             //Creating snake's head with '0'
         }
     }
+    //Function for updating the game atate
+    void updateGame () {
+        int prevX=snakeTail[0];
+        int prevTailY=[0];
+        int prev2X,prevY;
+        snakeTailX[0]=x;
+        snakeTailY[0]=y;
+        for (int i=1;i<snakeTailLen;i++){
+            prev2X=snakeTailX[i];prev2Y=snakeTailY[i];snakeTailX[i]=prevX;snakeTailY[i]=prevY;prevX=prev2X;prevY=prev2Y;
+            switch (sDir){
+                case LEFT:
+                x--;
+                break;
+                case RIGHT:
+                x++;
+                break;
+                case UP:
+                y--;
+                break;
+                case DOWN:
+                y++;
+                break;
+            }
+            //Checks for snake's collision with the wall(|)
+            if (x>=width||x<0||y>=height||y<0)
+            isGameOver=true;
+            //Checks for collision with the tail(0)
+
+            for (int i=0;i<snakeTailLen;i++){
+                if (snakeTailX[i]==x&& snakeTailY[i]==y)
+                isGameOver=true;
+            }
+            //Checks for snake's collision with the food(#)
+            if (x==fruitCordX && y==fruitCordY){
+                playerScore+=10;
+                fruitCordX=rand()%width;
+                fruitCordY=rand()% height;
+                snakeTailLen++;
+            }
+        }
+        //Function to set the game difficulty level
+        
+    }
+
 
  }
  
